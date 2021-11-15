edc1cf7edd0a9f472b3270e36e0b62792b8acb9c
prince <A00229731@mycambrian.ca>
2021-11-14 19:16:30 -0500

    on work

---
+++ Interfaces/Task2Interfaces.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+
+
+namespace A4.Task2Interface
+{
+	interface ICarryPassengers
+	{
+		public string CarryPassengers();
+	}
+	class Ship
+	{
+		public double Displacement { get; }
+
+		public Ship(double displacement)
+		{
+			Displacement = displacement;
+		}
+
+		public string CarryPassengers()
+		{
+			return "This type of ship does not carry passengers.";
+		}
+
+		public override string ToString() => $"A ship that displaces {Displacement} tons of water"; 
+	}
+
+	class CruiseShip : Ship, ICarryPassengers
+	{
+		private int numPassengers;
+
+		public CruiseShip(double displacement, int numPassengers) : base(displacement)
+		{
+			this.numPassengers = numPassengers;
+		}
+
+		public string CarryPassengers()
+		{
+			return $"{base.ToString()} and is carrying {numPassengers} passengers on a Caribbean Cruise";
+		}
+
+		public override string ToString() => CarryPassengers();
+	}
+
+	class BigRig
+	{
+		public int NumTires;
+
+		public BigRig(int numTires)
+		{
+			NumTires = numTires;
+		}
+
+		public string CarryPassengers()
+		{
+			return "This type of vehicle does not carry passengers.";
+		}
+
+		public override string ToString() => $"A vehicle with {NumTires} tires"; 
+	}
+
+	class TourBus : BigRig, ICarryPassengers
+	{
+		private int numPassengers;
+
+		public TourBus(int numTires, int numPassengers) : base(numTires)
+		{
+			this.numPassengers = numPassengers;
+		}
+
+		public string CarryPassengers()
+		{
+			return $"{base.ToString()} and is carrying {numPassengers} passengers on a cross country tour.";
+		}
+
+		public override string ToString() => CarryPassengers();
+	}
+}
